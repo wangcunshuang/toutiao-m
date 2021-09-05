d2fe7b7d4ce6b72b6ad6f681ab33a2ab34d0f71a
robert <robert.wcs@outlook.com>
2021-09-05 10:57:18 +0800

    项目初始化-测试Vant 组件库

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './styles/index.less'
 
 import Vant from 'vant'
 import 'vant/lib/index.css'
+
+import './styles/index.less'
 
 import 'amfe-flexible'
 
